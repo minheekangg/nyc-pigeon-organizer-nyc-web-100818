e2e4331e2c0deeab8080c86f07a4fd09b7c3a7c4
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 21:39:36 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, values|
     values.each do |v_label, names|
       names.each do |name|
-        temp << [key, name]
+        temp << [{key => v_label_name}]
         puts temp
       end
     end
