57ae99fb490e811547ac9b8d0d9294881ebea6f8
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 22:07:30 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
   temp = []
-  data.each_with_object({}) do |(key, h.values), |
+  data.each_with_object({}) do |(key, h, v), |
   puts key
   puts h
   end
